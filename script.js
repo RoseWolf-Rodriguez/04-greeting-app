d3ce3a967157988157695ba7c7c98fe20c9695c9
Lucretia Teretto <lteretto@asu.edu>
2025-06-16 03:20:06 +0000

    Add welcome message functionality with button and event handler

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var userName = "Friend";
+let userName = "Friend";
 console.log("Hello, " + userName + "!" );
+
+// Get the button and message elements from the page
+let welcomeBtn = document.getElementById("welcomeBtn");
+let welcomeMsg = document.getElementById("welcomeMsg");
+
+// Add a click event to the button
+welcomeBtn.onclick = function() {
+  // Show the welcome message using userName
+  welcomeMsg.textContent = "Welcome, " + userName + "!";
+};
